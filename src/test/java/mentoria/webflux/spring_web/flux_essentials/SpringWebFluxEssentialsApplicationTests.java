a61180c6eb030b2a2392e43a5a690a79c018c1a0
IsaUrie <88585867+IsaUrie@users.noreply.github.com>
2024-06-19 14:56:33 -0300

    Configurações iniciais
    Primeiro commit, configurações iniciais

---
+++ src/test/java/mentoria/webflux/spring_web/flux_essentials/SpringWebFluxEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package mentoria.webflux.spring_web.flux_essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
