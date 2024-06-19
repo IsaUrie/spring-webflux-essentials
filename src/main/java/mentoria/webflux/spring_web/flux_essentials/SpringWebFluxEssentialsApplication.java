30b34de9256ba0ca6db21c8a3bd4ebfcb0e2b06e
IsaUrie <88585867+IsaUrie@users.noreply.github.com>
2024-06-19 15:51:48 -0300

    Adiciona arquivo docker-compose

--- src/main/java/mentoria/webflux/spring_web/flux_essentials/SpringWebFluxEssentialsApplication.java
+++ src/main/java/mentoria/webflux/spring_web/flux_essentials/SpringWebFluxEssentialsApplication.java
@@ -8,7 +8,6 @@ public class SpringWebFluxEssentialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebFluxEssentialsApplication.class, args);
-		System.out.println("teste-git");
 	}
 
 }
